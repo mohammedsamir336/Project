553a66cfe50beb221ec4228e9a61d2d5b3a25f0f
mohammedsamir336 <58854203+mohammedsamir336@users.noreply.github.com>
2020-02-07 16:11:51 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost:8080/mm336/mm/public/'),
+    'url' => env('APP_URL', 'http://localhost/Project/public/'),
 
     'asset_url' => env('ASSET_URL', null),
 
