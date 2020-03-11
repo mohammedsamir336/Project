b52854985862445dd4347aa2a8e63849bfed78f4
mohammed samir <mohammed_samir336@yahoo.com>
2020-03-11 16:46:56 +0200

    updating

--- storage/framework/views/67e3c0285e676116b9ed67702e211e8225106c60.php
+++ storage/framework/views/67e3c0285e676116b9ed67702e211e8225106c60.php
@@ -184,7 +184,7 @@
     <main class="masthead">
         <div class="container d-flex h-100 align-items-center">
             <div class="mx-auto text-center">
-                <h1 class="mx-auto my-0 text-uppercase">asdsa</h1>
+                <h1 class="mx-auto my-0 text-uppercase">welcome</h1>
                 <h2 class="text-white-50 mx-auto mt-2 mb-5"> Mohammed Website created by <br>Mohammed Samir.</h2>
                 <a href="<?php echo e(route('home')); ?>" class="btn btn-primary js-scroll-trigger">Get Started</a>
             </div>
